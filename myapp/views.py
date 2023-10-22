69f84896c78bc6d330abc3161549e55c898ee7aa
irisg <irisgjoni@hotmail.co.uk>
2023-10-22 19:53:36 +0100

    added first few web pages

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.views import View
+from django.shortcuts import render
+
+
+class HelloWorld(View):
+    def get(self, request):
+        return HttpResponse("Hello, World!")
+
+
+class PageTwo(View):
+    def get(self, request):
+        return HttpResponse("page two !")
+
+
+def PageThree(request):
+    return render(request, 'myapp/Navigations.html')
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from myapp.views import HelloWorld, PageTwo, PageThree
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HelloWorld.as_view(), name='hello_world_cbv'),
+    path('two', PageTwo.as_view(), name='PageTwo_cbv'),
+    path('PageThree/', PageThree, name='PageThree'),
 ]
